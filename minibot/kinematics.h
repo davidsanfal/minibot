4db93afe35569d7aed65b113d8d427b79c9b5e55
David <david.sanchez.falero@gmail.com>
2015-06-03 20:09:11 +0200

    new models

--- minibot/kinematics.h
+++ minibot/kinematics.h
@@ -13,13 +13,16 @@ float radius_rotation(float wheelbase, float dist_l, float dist_r) {
 
 
 float angle_rotated(float wheelbase, float dist_l, float dist_r) {
-  return (dist_r - dist_l) / wheelbase;
+  return ( dist_l - dist_r) / wheelbase;
 }
 
 Position relative_movement(float wheelbase, float dist_l, float dist_r) {
   Position rel_pos;
+  float dist = (dist_l + dist_r)/2
   float angle = angle_rotated(wheelbase, dist_l, dist_r);
+  if(dist_l != dist_r){
   float radius = radius_rotation(wheelbase, dist_l, dist_r);
+  }
   rel_pos.angle = angle;
   rel_pos.pos.x = radius * cos(angle);
   rel_pos.pos.y = radius * sin(angle);
